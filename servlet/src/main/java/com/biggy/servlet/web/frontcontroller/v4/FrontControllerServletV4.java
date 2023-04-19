55bc51ad8041174e28d3205d2a4c35fff468406c
pythonstrup <pythonstrup@gmail.com>
2023-04-19 23:14:58 +0900

    controller refactor

--- servlet/src/main/java/com/biggy/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
+++ servlet/src/main/java/com/biggy/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
@@ -1,10 +1,9 @@
-package com.biggy.servlet.web.frontcontroller.v3;
+package com.biggy.servlet.web.frontcontroller.v4;
 
-import com.biggy.servlet.web.frontcontroller.ModelView;
 import com.biggy.servlet.web.frontcontroller.MyView;
-import com.biggy.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
-import com.biggy.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
-import com.biggy.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import com.biggy.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import com.biggy.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import com.biggy.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,15 +13,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "frontControllerServletV3", urlPatterns = "/front-controller/v3/*")
-public class FrontControllerServletV3 extends HttpServlet {
+@WebServlet(name = "frontControllerServletV4", urlPatterns = "/front-controller/v4/*")
+public class FrontControllerServletV4 extends HttpServlet {
 
-  private Map<String, ControllerV3> controllerMap = new HashMap<>();
+  private Map<String, ControllerV4> controllerMap = new HashMap<>();
 
-  public FrontControllerServletV3() {
-    controllerMap.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
-    controllerMap.put("/front-controller/v3/members/save", new MemberSaveControllerV3());
-    controllerMap.put("/front-controller/v3/members", new MemberListControllerV3());
+  public FrontControllerServletV4() {
+    controllerMap.put("/front-controller/v4/members/new-form", new MemberFormControllerV4());
+    controllerMap.put("/front-controller/v4/members/save", new MemberSaveControllerV4());
+    controllerMap.put("/front-controller/v4/members", new MemberListControllerV4());
   }
 
   @Override
@@ -30,7 +29,7 @@ public class FrontControllerServletV3 extends HttpServlet {
       throws ServletException, IOException {
     String requestURI = request.getRequestURI();
 
-    ControllerV3 controller = controllerMap.get(requestURI);
+    ControllerV4 controller = controllerMap.get(requestURI);
     if (controller == null) {
       response.setStatus(HttpServletResponse.SC_NOT_FOUND);
       return;
@@ -38,12 +37,12 @@ public class FrontControllerServletV3 extends HttpServlet {
 
     // paramMap
     Map<String, String> paramMap = createParamMap(request);
-    ModelView mv = controller.process(paramMap);
+    Map<String ,Object> model = new HashMap<>(); // 추가된 부분
+    String viewName = controller.process(paramMap, model);
 
-    String viewName = mv.getViewName();// 논리 이름 ex) new-form
     MyView view = viewResolver(viewName);
 
-    view.render(mv.getModel(), request, response);
+    view.render(model, request, response);
   }
 
   private MyView viewResolver(String viewName) {
