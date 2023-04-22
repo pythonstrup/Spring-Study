a229cdf0a689451c07210296f215f9d74f297e34
pythonstrup <pythonstrup@gmail.com>
2023-04-22 22:21:03 +0900

    view resolver

--- servlet/src/main/java/com/biggy/servlet/ServletApplication.java
+++ servlet/src/main/java/com/biggy/servlet/ServletApplication.java
@@ -3,6 +3,9 @@ package com.biggy.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan // 서블릿 자동 등록
 @SpringBootApplication
@@ -12,4 +15,9 @@ public class ServletApplication {
     SpringApplication.run(ServletApplication.class, args);
   }
 
+  // 스프링 Boot가 자동으로 해주는 것
+//  @Bean
+//  InternalResourceViewResolver internalResourceViewResolver() {
+//    return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+//  }
 }
