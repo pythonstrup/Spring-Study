8e864bd0c2e1962e87a3bd2db13c3e4fe07199ac
pythonstrup <pythonstrup@gmail.com>
2023-04-29 18:26:02 +0900

    @ModelAttribute

--- springmvc/src/main/java/com/biggy/springmvc/basic/request/RequestParamController.java
+++ springmvc/src/main/java/com/biggy/springmvc/basic/request/RequestParamController.java
@@ -1,11 +1,13 @@
 package com.biggy.springmvc.basic.request;
 
+import com.biggy.springmvc.basic.HelloData;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,7 +27,7 @@ public class RequestParamController {
 
   @ResponseBody
   @RequestMapping(value = "request-param-v2")
-  public String RequestParamV2(
+  public String requestParamV2(
       @RequestParam("username") String memberName,
       @RequestParam("age") int memberAge
   ) {
@@ -35,7 +37,7 @@ public class RequestParamController {
 
   @ResponseBody
   @RequestMapping(value = "request-param-v3")
-  public String RequestParamV3(
+  public String requestParamV3(
       @RequestParam String username,
       @RequestParam int age
   ) {
@@ -45,7 +47,7 @@ public class RequestParamController {
 
   @ResponseBody
   @RequestMapping(value = "request-param-v4")
-  public String RequestParamV4(String username, int age) {
+  public String requestParamV4(String username, int age) {
     log.info("username={}, age={}", username, age);
     return "ok";
   }
@@ -53,7 +55,7 @@ public class RequestParamController {
   // required는 default가 false이다.
   @ResponseBody
   @RequestMapping(value = "request-param-required")
-  public String RequestParamRequired(
+  public String requestParamRequired(
       @RequestParam(required = true) String username,
       @RequestParam(required = false) Integer age
   ) {
@@ -65,7 +67,7 @@ public class RequestParamController {
   // 또한 빈 문자열이 들어왔을 때 default 값이 들어온다.
   @ResponseBody
   @RequestMapping(value = "request-param-default")
-  public String RequestParamDefault(
+  public String requestParamDefault(
       @RequestParam(defaultValue = "guest") String username,
       @RequestParam(defaultValue = "-1") int age
   ) {
@@ -75,8 +77,22 @@ public class RequestParamController {
 
   @ResponseBody
   @RequestMapping(value = "request-param-map")
-  public String RequestParamMap(@RequestParam Map<String, Object> paramMap) {
+  public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
     log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+    return "ok";
+  }
+
+  @ResponseBody
+  @RequestMapping(value = "/model-attribute-v1")
+  public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+    log.info("username = {}, age = {}", helloData.getUsername(), helloData.getAge());
+    return "ok";
+  }
+
+  @ResponseBody
+  @RequestMapping(value = "/model-attribute-v2")
+  public String modelAttributeV2(HelloData helloData) {
+    log.info("username = {}, age = {}", helloData.getUsername(), helloData.getAge());
     return "ok";
   }
 }
