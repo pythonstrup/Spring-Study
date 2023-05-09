08b499c68d886de877065ab32306c293ba57909c
pythonstrup <pythonstrup@gmail.com>
2023-05-09 22:07:02 +0900

    embedded tomcat - servlet

---
+++ spring-boot/embed/src/main/java/hello/embed/EmbedTomcatServletMain.java
@@ -0,0 +1,26 @@
+package hello.embed;
+
+import hello.servlet.HelloServlet;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+
+public class EmbedTomcatServletMain {
+
+  public static void main(String[] args) throws LifecycleException {
+    System.out.println("EmbedTomcatServletMain.main");
+
+    // Tomcat Settings
+    Tomcat tomcat = new Tomcat();
+    Connector connector = new Connector();
+    connector.setPort(8080);
+    tomcat.setConnector(connector);
+
+    // register servlet
+    Context context = tomcat.addContext("", "/");
+    tomcat.addServlet("", "helloServlet", new HelloServlet());
+    context.addServletMappingDecoded("/hello-servlet", "helloServlet");
+    tomcat.start();
+  }
+}
