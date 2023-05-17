89d4b03e8a5fcafd5a7ca31c3be623be0cae3cc7
pythonstrup <pythonstrup@gmail.com>
2023-05-17 09:34:15 +0900

    @ConfigurationProperties constructor(remove setter)

--- spring-boot/external-read/src/main/java/hello/ExternalReadApplication.java
+++ spring-boot/external-read/src/main/java/hello/ExternalReadApplication.java
@@ -1,6 +1,7 @@
 package hello;
 
 import hello.config.MyDataSourceConfigV1;
+import hello.config.MyDataSourceConfigV2;
 import hello.config.MyDataSourceEnvConfig;
 import hello.config.MyDataSourceValueConfig;
 import org.springframework.boot.SpringApplication;
@@ -10,9 +11,10 @@ import org.springframework.context.annotation.Import;
 
 //@Import(MyDataSourceEnvConfig.class)
 //@Import(MyDataSourceValueConfig.class)
-@Import(MyDataSourceConfigV1.class)
+//@Import(MyDataSourceConfigV1.class)
+@Import(MyDataSourceConfigV2.class)
 @SpringBootApplication(scanBasePackages = "hello.datasource")
-//@ConfigurationPropertiesScan // @ConfigurationProperties 전체 스캔
+//@ConfigurationPropertiesScan // @ConfigurationProperties 전체 스캔 => @EnableConfigurationProperties 대체
 @ConfigurationPropertiesScan({"hello"}) // 패키지 지정 가능
 public class ExternalReadApplication {
 
