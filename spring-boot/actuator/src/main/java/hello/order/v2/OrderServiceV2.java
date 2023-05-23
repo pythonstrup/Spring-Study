0ad9f783d8ba7ffa27d2b0fd4978dd05f6649f4c
pythonstrup <pythonstrup@gmail.com>
2023-05-23 21:43:02 +0900

    Metric @Counted

---
+++ spring-boot/actuator/src/main/java/hello/order/v2/OrderServiceV2.java
@@ -0,0 +1,30 @@
+package hello.order.v2;
+
+import hello.order.OrderService;
+import io.micrometer.core.annotation.Counted;
+import java.util.concurrent.atomic.AtomicInteger;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class OrderServiceV2 implements OrderService {
+  private AtomicInteger stock = new AtomicInteger(100);
+
+  @Counted("my.order")
+  @Override
+  public void order() {
+    log.info("주문");
+    stock.decrementAndGet();
+  }
+
+  @Counted("my.order")
+  @Override
+  public void cancel() {
+    log.info("취소");
+    stock.incrementAndGet();
+  }
+
+  @Override
+  public AtomicInteger getStock() {
+    return stock;
+  }
+}
