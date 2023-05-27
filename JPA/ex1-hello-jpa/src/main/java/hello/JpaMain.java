4221a37e189c9aac7cbb31153a30b0afec6d9a62
pythonstrup <pythonstrup@gmail.com>
2023-05-27 15:10:33 +0900

    PK Mapping

--- JPA/ex1-hello-jpa/src/main/java/hello/JpaMain.java
+++ JPA/ex1-hello-jpa/src/main/java/hello/JpaMain.java
@@ -17,11 +17,22 @@ public class JpaMain {
     tx.begin();
 
     try {
-      Member member = new Member();
-      member.setUsername("A");
+      Member member1 = new Member();
+      member1.setUsername("A");
 
-      em.persist(member);
+      Member member2 = new Member();
+      member2.setUsername("B");
 
+      Member member3 = new Member();
+      member3.setUsername("C");
+
+      System.out.println("===========================");
+      em.persist(member1);
+      em.persist(member2);
+      em.persist(member3);
+      System.out.println("member.id = " + member1.getId());
+      System.out.println("member.id = " + member2.getId());
+      System.out.println("member.id = " + member3.getId());
       System.out.println("===========================");
 
       tx.commit();
