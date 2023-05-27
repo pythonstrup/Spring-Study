67ec4eda4376eb2728c730b6b3eeda830773e996
pythonstrup <pythonstrup@gmail.com>
2023-05-27 11:47:06 +0900

    field and column mapping

--- JPA/ex1-hello-jpa/src/main/java/com/hello/JpaMain.java
+++ JPA/ex1-hello-jpa/src/main/java/com/hello/JpaMain.java
@@ -17,7 +17,12 @@ public class JpaMain {
     tx.begin();
 
     try {
+      Member member = new Member();
+      member.setId(2L);
+      member.setUsername("B");
+      member.setRoleType(RoleType.USER);
 
+      em.persist(member);
 
       System.out.println("===========================");
 
@@ -44,34 +49,34 @@ public class JpaMain {
 //      findMember.setName("new Kim");
 
     // JPQL
-    String jpql = "select m from Member as m where m.name like '%park%'";
-    List<Member> resultList = em.createQuery(jpql, Member.class)
-        .setFirstResult(0) // offset - pagination
-        .setMaxResults(2)  // limit - pagination
-        .getResultList();
-    for (Member member : resultList) {
-      System.out.println("member.name = " + member.getName());
-    }
+//    String jpql = "select m from Member as m where m.name like '%park%'";
+//    List<Member> resultList = em.createQuery(jpql, Member.class)
+//        .setFirstResult(0) // offset - pagination
+//        .setMaxResults(2)  // limit - pagination
+//        .getResultList();
+//    for (Member member : resultList) {
+//      System.out.println("member.name = " + member.getName());
+//    }
   }
 
   private void persist1(EntityManager em) {
     // 비영속
-    Member member = new Member();
-    member.setId(101L);
-    member.setName("HelloJPA");
+//    Member member = new Member();
+//    member.setId(101L);
+//    member.setName("HelloJPA");
 
     // 영속
-    System.out.println("===BEFORE===");
-    em.persist(member);
+//    System.out.println("===BEFORE===");
+//    em.persist(member);
     // em.detach(member); // 영속에서 분리 -> 준영속 상태
     // em.remove(member); // 객체를 삭제한 상태
-    System.out.println("===AFTER===");
-
-    // 조회를 하는데, select 쿼리를 날리지 않는다!! => 1차 캐시를 먼저 조회하기 때문이다.
-    Member findMember = em.find(Member.class, 101L);
-
-    System.out.println("findMember.id = " + findMember.getId());
-    System.out.println("findMember.name = " + findMember.getName());
+//    System.out.println("===AFTER===");
+//
+//    // 조회를 하는데, select 쿼리를 날리지 않는다!! => 1차 캐시를 먼저 조회하기 때문이다.
+//    Member findMember = em.find(Member.class, 101L);
+//
+//    System.out.println("findMember.id = " + findMember.getId());
+//    System.out.println("findMember.name = " + findMember.getName());
   }
 
   private void persist2(EntityManager em) {
@@ -85,30 +90,30 @@ public class JpaMain {
 //      em.persist(member1);
 //      em.persist(member2);
 
-    Member member = em.find(Member.class, 150L);
-    member.setName("ZZZZZZZ");
+//    Member member = em.find(Member.class, 150L);
+//    member.setName("ZZZZZZZ");
     System.out.println("=======================");
   }
 
   private void flush(EntityManager em) {
-    Member member = new Member(200L, "member200");
-    em.persist(member);
+//    Member member = new Member(200L, "member200");
+//    em.persist(member);
 
-    em.flush(); // 쿼리가 즉시 실행된다.
+//    em.flush(); // 쿼리가 즉시 실행된다.
     // 영속성 컨텍스트를 비우는 게 아니라, 변경내용을 데이터베이스에 동기화하는 작업이다.
 
     System.out.println("===========================");
   }
 
   private void semiPersistence(EntityManager em) {
-    Member member1 = em.find(Member.class, 150L);
-    member1.setName("AAAAAAAA");
+//    Member member1 = em.find(Member.class, 150L);
+//    member1.setName("AAAAAAAA");
 
     // em.detach(member1); // 특정 엔티티만 준영속 상태로 전환한다.
-    em.clear(); // 영속성 컨텍스트를 통으로 다 지워버린다. 그러므로 1차 캐시가 작동하지 않고 다시 select하게 된다.
+//    em.clear(); // 영속성 컨텍스트를 통으로 다 지워버린다. 그러므로 1차 캐시가 작동하지 않고 다시 select하게 된다.
     // em.close(); // 영속성 컨텍스트를 아예 닫아버린다.
 
-    Member member2 = em.find(Member.class, 150L);
+//    Member member2 = em.find(Member.class, 150L);
 
     System.out.println("===========================");
   }
--- JPA/ex1-hello-jpa/src/main/java/com/hello/Member.java
+++ JPA/ex1-hello-jpa/src/main/java/com/hello/Member.java
@@ -1,9 +1,7 @@
 package com.hello;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import java.util.Date;
+import javax.persistence.*;
 
 @Entity
 // @Table(name = "MBR")
@@ -12,15 +10,30 @@ public class Member {
   @Id
   private Long id;
 
-  @Column(unique = true, length = 10)
-  private String name;
+  @Column(name = "name",
+      nullable = false,
+      columnDefinition = "varchar(100) default 'EMPTY'")
+  private String username;
+
+  private Integer age;
+
+  @Enumerated(EnumType.STRING)
+  private RoleType roleType;
+
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date createDate;
+
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date lastModifiedDate;
+
+  // DB에 적용하지 않고 메모리에서만 사용하고 싶을 때
+  @Transient
+  private int temp;
+
+  @Lob
+  private String description;
 
   public Member() {}
-
-  public Member(Long id, String name) {
-    this.id = id;
-    this.name = name;
-  }
 
   public Long getId() {
     return id;
@@ -30,11 +43,59 @@ public class Member {
     this.id = id;
   }
 
-  public String getName() {
-    return name;
+  public String getUsername() {
+    return username;
   }
 
-  public void setName(String name) {
-    this.name = name;
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+  public RoleType getRoleType() {
+    return roleType;
+  }
+
+  public void setRoleType(RoleType roleType) {
+    this.roleType = roleType;
+  }
+
+  public Date getCreateDate() {
+    return createDate;
+  }
+
+  public void setCreateDate(Date createDate) {
+    this.createDate = createDate;
+  }
+
+  public Date getLastModifiedDate() {
+    return lastModifiedDate;
+  }
+
+  public void setLastModifiedDate(Date lastModifiedDate) {
+    this.lastModifiedDate = lastModifiedDate;
+  }
+
+  public int getTemp() {
+    return temp;
+  }
+
+  public void setTemp(int temp) {
+    this.temp = temp;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
   }
 }
