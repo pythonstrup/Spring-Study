1b03a696c7b341bad75e39018912534801c327e5
pythonstrup <pythonstrup@gmail.com>
2023-05-27 16:53:12 +0900

    ManyToOne(one direction)

--- JPA/ex1-hello-jpa/src/main/java/hello/JpaMain.java
+++ JPA/ex1-hello-jpa/src/main/java/hello/JpaMain.java
@@ -17,23 +17,7 @@ public class JpaMain {
     tx.begin();
 
     try {
-      Member member1 = new Member();
-      member1.setUsername("A");
 
-      Member member2 = new Member();
-      member2.setUsername("B");
-
-      Member member3 = new Member();
-      member3.setUsername("C");
-
-      System.out.println("===========================");
-      em.persist(member1);
-      em.persist(member2);
-      em.persist(member3);
-      System.out.println("member.id = " + member1.getId());
-      System.out.println("member.id = " + member2.getId());
-      System.out.println("member.id = " + member3.getId());
-      System.out.println("===========================");
 
       tx.commit();
     } catch (Exception e) {
@@ -125,5 +109,43 @@ public class JpaMain {
 //    Member member2 = em.find(Member.class, 150L);
 
     System.out.println("===========================");
+  }
+
+  private void pk(EntityManager em) {
+//    Member member1 = new Member();
+//    member1.setUsername("A");
+//
+//    Member member2 = new Member();
+//    member2.setUsername("B");
+//
+//    Member member3 = new Member();
+//    member3.setUsername("C");
+//
+//    System.out.println("===========================");
+//    em.persist(member1);
+//    em.persist(member2);
+//    em.persist(member3);
+//    System.out.println("member.id = " + member1.getId());
+//    System.out.println("member.id = " + member2.getId());
+//    System.out.println("member.id = " + member3.getId());
+//    System.out.println("===========================");
+  }
+
+  private void manyToOne(EntityManager em) {
+    Team team = new Team();
+    team.setName("TeamA");
+    em.persist(team);
+
+    Member member = new Member();
+    member.setUsername("member1");
+    member.setTeam(team);
+    em.persist(member);
+
+    em.flush();
+    em.clear();
+
+    Member findMember = em.find(Member.class, member.getId());
+    Team findTeam = member.getTeam();
+    System.out.println("findTeam = " + findTeam.getName());
   }
 }
--- JPA/ex1-hello-jpa/src/main/java/hello/Member.java
+++ JPA/ex1-hello-jpa/src/main/java/hello/Member.java
@@ -4,27 +4,21 @@ import java.util.Date;
 import javax.persistence.*;
 
 @Entity
-// @Table(name = "MBR")
-// call next value를 얼마나 한 번에 가져올지 설정이 allocationSize이다.
-@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq", initialValue = 1, allocationSize = 50) // SEQ 이름 지정
-//@TableGenerator(name = "MEMBER_SEQ_GENERATOR", table = "MY_SEQUENCES", pkColumnName = "MEMBER_SEQ", allocationSize = 1)
 public class Member {
 
-//  @GeneratedValue(strategy = GenerationType.IDENTITY) // ex) MySQL autoincrement
-  // `call next value for {sequence명}` 으로 마지막 SEQ를 가져와 그 다음으로 업데이트쳐줌 => Oracle에서 많이 사용
-  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
-  // 테이블을 확인 => SEQ 전략과 비슷 그러나 성능이 안 좋다. 운영에서 사용하기엔 부담스럽다.
-//  @GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR")
-  @Id
+  @Id @GeneratedValue
+  @Column(name = "MEMBER_ID")
   private Long id;
 
-  @Column(name = "name",
-      nullable = false,
-      columnDefinition = "varchar(100) default 'EMPTY'")
+  @Column(name = "USERNAME")
   private String username;
 
-  public Member() {
-  }
+//  @Column(name = "TEAM_ID")
+//  private Long teamId;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "TEAM_ID")
+  private Team team;
 
   public Long getId() {
     return id;
@@ -40,5 +34,13 @@ public class Member {
 
   public void setUsername(String username) {
     this.username = username;
+  }
+
+  public Team getTeam() {
+    return team;
+  }
+
+  public void setTeam(Team team) {
+    this.team = team;
   }
 }
