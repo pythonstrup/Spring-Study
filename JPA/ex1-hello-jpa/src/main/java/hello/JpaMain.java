8fc9b7340e7d40ae7eced6b4fd00e9ae7d7510bd
pythonstrup <pythonstrup@gmail.com>
2023-05-31 01:11:22 +0900

    Proxy Object

--- JPA/ex1-hello-jpa/src/main/java/hello/JpaMain.java
+++ JPA/ex1-hello-jpa/src/main/java/hello/JpaMain.java
@@ -6,6 +6,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.PersistenceUnitUtil;
+import org.hibernate.Hibernate;
+import org.hibernate.jpa.internal.PersistenceUnitUtilImpl;
 
 public class JpaMain {
 
@@ -18,185 +21,16 @@ public class JpaMain {
     tx.begin();
 
     try {
-      Member member = new Member();
-      member.setUsername("user1");
-      member.setCreatedBy("Park");
-      member.setCreatedDate(LocalDateTime.now());
 
-      em.persist(member);
 
       tx.commit();
     } catch (Exception e) {
+      e.printStackTrace();
       tx.rollback();
     } finally {
       em.close();
     }
 
     emf.close();
-  }
-
-  private void basic(EntityManager em) {
-    // save
-//      Member member = new Member();
-//      member.setId(3L);
-//      member.setName("park park");
-//      em.persist(member);
-
-    // em.persist를 호출하지 않아도 자동으로 변경된다.(update문 실행됨)
-    // 더티 체킹!! 영속성 컨텍스트!!
-//      Member findMember = em.find(Member.class, 1L);
-//      findMember.setName("new Kim");
-
-    // JPQL
-//    String jpql = "select m from Member as m where m.name like '%park%'";
-//    List<Member> resultList = em.createQuery(jpql, Member.class)
-//        .setFirstResult(0) // offset - pagination
-//        .setMaxResults(2)  // limit - pagination
-//        .getResultList();
-//    for (Member member : resultList) {
-//      System.out.println("member.name = " + member.getName());
-//    }
-  }
-
-  private void persist1(EntityManager em) {
-    // 비영속
-//    Member member = new Member();
-//    member.setId(101L);
-//    member.setName("HelloJPA");
-
-    // 영속
-//    System.out.println("===BEFORE===");
-//    em.persist(member);
-    // em.detach(member); // 영속에서 분리 -> 준영속 상태
-    // em.remove(member); // 객체를 삭제한 상태
-//    System.out.println("===AFTER===");
-//
-//    // 조회를 하는데, select 쿼리를 날리지 않는다!! => 1차 캐시를 먼저 조회하기 때문이다.
-//    Member findMember = em.find(Member.class, 101L);
-//
-//    System.out.println("findMember.id = " + findMember.getId());
-//    System.out.println("findMember.name = " + findMember.getName());
-  }
-
-  private void persist2(EntityManager em) {
-    // 결과적으로 쿼리는 1번 나간다.
-//      Member findMember1 = em.find(Member.class, 101L);
-//      Member findMember2 = em.find(Member.class, 101L); // 1차 캐시로 가지고 온다.
-//      System.out.println("result = " + (findMember1 == findMember2));
-
-//      Member member1 = new Member(150L, "A");
-//      Member member2 = new Member(160L, "B");
-//      em.persist(member1);
-//      em.persist(member2);
-
-//    Member member = em.find(Member.class, 150L);
-//    member.setName("ZZZZZZZ");
-    System.out.println("=======================");
-  }
-
-  private void flush(EntityManager em) {
-//    Member member = new Member(200L, "member200");
-//    em.persist(member);
-
-//    em.flush(); // 쿼리가 즉시 실행된다.
-    // 영속성 컨텍스트를 비우는 게 아니라, 변경내용을 데이터베이스에 동기화하는 작업이다.
-
-    System.out.println("===========================");
-  }
-
-  private void semiPersistence(EntityManager em) {
-//    Member member1 = em.find(Member.class, 150L);
-//    member1.setName("AAAAAAAA");
-
-    // em.detach(member1); // 특정 엔티티만 준영속 상태로 전환한다.
-//    em.clear(); // 영속성 컨텍스트를 통으로 다 지워버린다. 그러므로 1차 캐시가 작동하지 않고 다시 select하게 된다.
-    // em.close(); // 영속성 컨텍스트를 아예 닫아버린다.
-
-//    Member member2 = em.find(Member.class, 150L);
-
-    System.out.println("===========================");
-  }
-
-  private void pk(EntityManager em) {
-//    Member member1 = new Member();
-//    member1.setUsername("A");
-//
-//    Member member2 = new Member();
-//    member2.setUsername("B");
-//
-//    Member member3 = new Member();
-//    member3.setUsername("C");
-//
-//    System.out.println("===========================");
-//    em.persist(member1);
-//    em.persist(member2);
-//    em.persist(member3);
-//    System.out.println("member.id = " + member1.getId());
-//    System.out.println("member.id = " + member2.getId());
-//    System.out.println("member.id = " + member3.getId());
-//    System.out.println("===========================");
-  }
-
-  private void manyToOne(EntityManager em) {
-//    Team team = new Team();
-//    team.setName("TeamA");
-//    em.persist(team);
-//
-//    Member member = new Member();
-//    member.setUsername("member1");
-//    member.setTeam(team);
-//    em.persist(member);
-//
-//    em.flush();
-//    em.clear();
-//
-//    Member findMember = em.find(Member.class, member.getId());
-//    Team findTeam = member.getTeam();
-//    System.out.println("findTeam = " + findTeam.getName());
-  }
-
-  public void relation(EntityManager em) {
-//    Team team = new Team();
-//    team.setName("TeamA");
-//    em.persist(team);
-//
-//    Locker locker = new Locker();
-//    locker.setName("Locker1");
-//    em.persist(locker);
-//
-//    Member member = new Member();
-//    member.setUsername("member1");
-//    member.addLocker(locker);
-////      member.changeTeam(team);
-//    team.addMember(member);
-//    em.persist(member);
-//
-////      em.flush();
-////      em.clear();
-//
-//    Team findTeam = em.find(Team.class, team.getId());
-//    List<Member> members = findTeam.getMembers();
-//
-//    System.out.println("======================");
-////      System.out.println(findTeam); // toString으로 인한 순환참조 문제
-//    for (Member m: members) {
-//      System.out.println("m = " + m.getUsername());
-//    }
-//    System.out.println("======================");
-  }
-
-  public void extension(EntityManager em) {
-    Movie movie = new Movie();
-    movie.setDirector("Perter Jackson");
-    movie.setActor("Mortensen");
-    movie.setName("Lord of the rings");
-    movie.setPrice(15000);
-
-    em.persist(movie);
-    em.flush();
-    em.clear();
-
-    Movie findMovie = em.find(Movie.class, movie.getId());
-    System.out.println("findMove = " + findMovie);
   }
 }
---
+++ JPA/ex1-hello-jpa/src/main/java/hello/lecture/Proxy.java
@@ -0,0 +1,133 @@
+package hello.lecture;
+
+import hello.Member;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import org.hibernate.Hibernate;
+
+public class Proxy {
+
+  private void proxy1(EntityManager em) {
+    Member member1 = new Member();
+    member1.setUsername("user1");
+    em.persist(member1);
+
+    em.flush();
+    em.clear();
+
+//      Member findMember = em.find(Member.class, member.getId());
+    Member findMember1 = em.getReference(Member.class, member1.getId()); // 프록시 클래스 - 가짜 엔티티 객체 조회
+    System.out.println("before findMember = " + findMember1.getClass());
+    printMember(findMember1);
+    System.out.println("after findMember = " + findMember1.getClass()); // 프록시가 엔티티로 대체되지 않는다.
+  }
+
+  private static void printMember(Member member) {
+    String username = member.getUsername();
+    System.out.println("username = " + username);
+  }
+
+  // proxy의 instance
+  private void proxy2(EntityManager em) {
+    Member member1 = new Member();
+    member1.setUsername("user1");
+    em.persist(member1);
+
+    Member member2 = new Member();
+    member2.setUsername("user2");
+    em.persist(member2);
+
+    Member findMember2 = em.getReference(Member.class, member2.getId());
+    System.out.println("member1 == member2: " + (member1.getClass() == member2.getClass()));
+    System.out.println("member1 is Member: " + (member1 instanceof Member));
+    System.out.println("member2 is Member: " + (member2 instanceof Member));
+  }
+
+  // proxy가 entity로 대체되는 경우
+  private void proxyReplacedByEntity(EntityManager em) {
+    Member member1 = new Member();
+    member1.setUsername("user1");
+    em.persist(member1);
+
+    em.flush();
+    em.clear();
+
+    Member m1 = em.find(Member.class, member1.getId());
+    System.out.println("m1 = " + m1.getClass());
+
+    Member reference = em.getReference(Member.class, member1.getId());
+    System.out.println("reference = " + reference.getClass()); // 프록시가 엔티티로 대체된다.
+    System.out.println("m1 == reference: " + (m1 == reference));
+  }
+
+  // entity가 proxy로 대체되는 경우
+  private void entityReplacedByProxy(EntityManager em) {
+    Member member1 = new Member();
+    member1.setUsername("user1");
+    em.persist(member1);
+
+    em.flush();
+    em.clear();
+
+    Member reference = em.getReference(Member.class, member1.getId());
+    System.out.println("reference = " + reference.getClass());
+
+    Member m1 = em.find(Member.class, member1.getId()); // 오히려 find로 찾아오는 entity가 proxy로 대체되어 버린다.
+    System.out.println("m1 = " + m1.getClass()); //
+
+    System.out.println("m1 == reference: " + (m1 == reference));
+  }
+
+  // proxy는 영속성과 생명을 같이 한다.
+  private void proxyDependsOnPersistence(EntityManager em) {
+    Member member1 = new Member();
+    member1.setUsername("user1");
+    em.persist(member1);
+
+    em.flush();
+    em.clear();
+
+    Member reference = em.getReference(Member.class, member1.getId());
+    System.out.println("reference = " + reference.getClass());
+
+//      em.detach(reference);
+//      em.close();
+    em.clear();
+
+    System.out.println("reference = " + reference.getUsername());
+  }
+
+  // proxy가 loading 됐는지 확인하는 방법
+  private void persistenceUnitUtilIsLoaded(EntityManagerFactory emf, EntityManager em) {
+    Member member1 = new Member();
+    member1.setUsername("user1");
+    em.persist(member1);
+
+    em.flush();
+    em.clear();
+
+    Member reference = em.getReference(Member.class, member1.getId());
+    System.out.println("reference = " + reference.getClass());
+
+    System.out.println("Before isLoaded = " + emf.getPersistenceUnitUtil().isLoaded(reference)); // false
+
+    reference.getUsername(); // loading
+    System.out.println("After isLoaded = " + emf.getPersistenceUnitUtil().isLoaded(reference)); // true
+  }
+
+  // getter로 proxy를 강제초기화하는 것은 뭔가 어색해보인다.
+  // Hibernate의 initialize 메소드를 사용하면 강제초기화를 할 수 있다.
+  private void forceToInitialize(EntityManager em) {
+
+    Member member1 = new Member();
+    member1.setUsername("user1");
+    em.persist(member1);
+
+    em.flush();
+    em.clear();
+
+    Member reference = em.getReference(Member.class, member1.getId());
+    System.out.println("reference = " + reference.getClass());
+    Hibernate.initialize(reference); // 강제초기화 => select query가 발생한다.
+  }
+}
