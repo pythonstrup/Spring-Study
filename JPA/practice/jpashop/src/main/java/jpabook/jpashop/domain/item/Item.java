161e46122db9241f0a068859c3d82d788a44e97d
pythonstrup <pythonstrup@gmail.com>
2023-06-08 22:44:00 +0900

    Item Entity Business Logic

--- JPA/practice/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ JPA/practice/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -11,6 +11,7 @@ import jakarta.persistence.ManyToMany;
 import java.util.ArrayList;
 import java.util.List;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -30,4 +31,17 @@ public abstract class Item {
 
   @ManyToMany(mappedBy = "items")
   private List<Category> categories = new ArrayList<>();
+
+  //===Business Logic===//
+  public void addStock(int quantity) {
+    this.stockQuantity += quantity;
+  }
+
+  public void subtractStock(int quantity) {
+    int restStock = this.stockQuantity - quantity;
+    if (restStock < 0) {
+      throw new NotEnoughStockException("need more stock");
+    }
+    this.stockQuantity = restStock;
+  }
 }
