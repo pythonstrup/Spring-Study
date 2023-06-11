dd12bd51ced0d5f43bb417bb77e4c6423818a2d8
pythonstrup <pythonstrup@gmail.com>
2023-06-11 17:07:18 +0900

    Item Save

---
+++ JPA/practice/jpashop/src/main/java/jpabook/jpashop/dto/BookFormDto.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class BookFormDto {
+
+  private Long id;
+  private String name;
+  private int price;
+  private int stockQuantity;
+
+  private String author;
+  private String isbn;
+}
