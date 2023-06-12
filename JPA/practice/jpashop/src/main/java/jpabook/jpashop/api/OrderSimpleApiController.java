e021321a02a1e1548b63936743f1784bcd753b95
pythonstrup <pythonstrup@gmail.com>
2023-06-13 00:01:31 +0900

    Entity To DTO

--- JPA/practice/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ JPA/practice/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,9 +1,14 @@
 package jpabook.jpashop.api;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +36,35 @@ public class OrderSimpleApiController {
       return all;
     }
 
+    // START: ORDER -> SQL 1번 (결과 2건)
+    // LOOP1: MEMBER, DELIVERY
+    // LOOP2: MEMBER, DELIVERY
+    // 총 5번의 쿼리가 나간다. (1 + N + N 번 쿼리가 나간다.)
+    // EAGER로 해도 동일한 문제가 발생한다.
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+      List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+      List<SimpleOrderDto> result = orders.stream()
+          .map(SimpleOrderDto::new)
+          .collect(Collectors.toList());
 
+      return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+      private Long orderId;
+      private String name;
+      private LocalDateTime orderDate;
+      private OrderStatus orderStatus;
+      private Address address;
+
+      public SimpleOrderDto(Order order) {
+        orderId = order.getId();
+        name = order.getMember().getUsername(); // LAZY 초기화
+        orderDate = order.getOrderDate();
+        orderStatus = order.getStatus();
+        address = order.getDelivery().getAddress(); // LAZY 초기화
+      }
+    }
 }
