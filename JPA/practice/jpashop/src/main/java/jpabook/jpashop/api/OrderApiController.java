02f13609daab868699b4c6d52eb7fb8c0bb38f5a
pythonstrup <pythonstrup@gmail.com>
2023-06-14 21:22:52 +0900

    Collection Select - Worst Practice

---
+++ JPA/practice/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.api;
+
+import java.util.List;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+  private final OrderRepository orderRepository;
+
+  // 바람직하지 않은 방법
+  @GetMapping("/api/v1/orders")
+  public List<Order> ordersV1() {
+    List<Order> all = orderRepository.findAllByString(new OrderSearch());
+    for (Order order : all) {
+      order.getMember().getUsername(); // LAZY 로딩 엔티티 강제 초기화
+      order.getDelivery().getAddress();
+      List<OrderItem> orderItems = order.getOrderItems();
+      orderItems.stream().forEach(o -> o.getItem().getName()); // orderItems도 초기화
+    }
+    return all;
+  }
+}
