800e0605ed3817eba5086ea51ec3e32e91d7b84c
pythonstrup <pythonstrup@gmail.com>
2023-06-18 00:08:20 +0900

    Data JPA Start

---
+++ JPA/datajpa/datajpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,32 @@
+package study.datajpa.repository;
+
+import static org.assertj.core.api.Assertions.as;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+@SpringBootTest
+@Transactional
+class MemberJpaRepositoryTest {
+
+  @Autowired
+  MemberJpaRepository memberJpaRepository;
+
+  @Test
+  void testMember() {
+    Member member = new Member("memberA");
+    Member savedMember = memberJpaRepository.save(member);
+
+    Member findMember = memberJpaRepository.find(savedMember.getId());
+
+    assertThat(findMember.getId()).isEqualTo(member.getId());
+    assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    assertThat(findMember).isEqualTo(member); // (영속성 컨텍스트) 1차캐시로 member를 조회해왔기 때문에 동일한 객체가 조회된다.
+  }
+}
