e49e0eb42157cc98fec30e7e525fec254029df34
pythonstrup <pythonstrup@gmail.com>
2023-07-01 14:16:37 +0900

    Sorting

--- JPA/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ JPA/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -142,4 +142,28 @@ public class QuerydslBasicTest {
 //        .fetchCount();
 //    assertThat(count).isEqualTo(4);
   }
+
+  /**
+   * age 내림차순
+   * username 오름차순 => username 없으면 마지막에 출력 (nulls last)
+   */
+  @Test
+  public void sort() {
+      em.persist(new Member(null, 50));
+    em.persist(new Member("member5", 50));
+    em.persist(new Member("member6", 50));
+
+    List<Member> result = queryFactory
+        .selectFrom(member)
+        .where(member.age.eq(50))
+        .orderBy(member.age.desc(), member.username.asc().nullsLast())
+        .fetch();
+
+    Member member5 = result.get(0);
+    Member member6 = result.get(1);
+    Member memberNull = result.get(2);
+    assertThat(member5.getUsername()).isEqualTo("member5");
+    assertThat(member6.getUsername()).isEqualTo("member6");
+    assertThat(memberNull.getUsername()).isNull();
+  }
 }
