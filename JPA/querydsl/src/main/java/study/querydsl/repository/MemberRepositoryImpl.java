783ac71f076c15cec927d12186530819276ce199
pythonstrup <pythonstrup@gmail.com>
2023-07-10 23:27:59 +0900

    QueryDSL - Dynamic Query with Paging

--- JPA/querydsl/src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ JPA/querydsl/src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -3,10 +3,15 @@ package study.querydsl.repository;
 import static study.querydsl.entity.QMember.member;
 import static study.querydsl.entity.QTeam.team;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Wildcard;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.util.StringUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
@@ -39,6 +44,72 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
             ageGoe(condition.getAgeGoe())
         )
         .fetch();
+  }
+
+  @Override
+  public Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable) {
+    // fetchResults는 deprecated 되었다.
+    // 복잡한 쿼리에 대해서 어려 오류 사례를 만들었기 때문이다.
+    // 따라서 아래 방식은 웬만하면 사용하지 말자.
+    QueryResults<MemberTeamDto> result = queryFactory
+        .select(new QMemberTeamDto(
+            member.id.as("memberId"),
+            member.username,
+            member.age,
+            team.id.as("teamId"),
+            team.name.as("teamName")
+        ))
+        .from(member)
+        .leftJoin(member.team, team)
+        .where(
+            usernameEq(condition.getUsername()),
+            teamNameEq(condition.getTeamName()),
+            ageLoe(condition.getAgeLoe()),
+            ageGoe(condition.getAgeGoe())
+        ).offset(pageable.getOffset())
+        .limit(pageable.getPageSize())
+        .fetchResults();
+
+    List<MemberTeamDto> content = result.getResults();
+    long total = result.getTotal();
+
+    return new PageImpl<>(content, pageable, total);
+  }
+
+  @Override
+  public Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable) {
+    // fetchResult 대신 아래와 같은 방법을 사용하는 것을 권장
+    List<MemberTeamDto> result = queryFactory
+        .select(new QMemberTeamDto(
+            member.id.as("memberId"),
+            member.username,
+            member.age,
+            team.id.as("teamId"),
+            team.name.as("teamName")
+        ))
+        .from(member)
+        .leftJoin(member.team, team)
+        .where(
+            usernameEq(condition.getUsername()),
+            teamNameEq(condition.getTeamName()),
+            ageLoe(condition.getAgeLoe()),
+            ageGoe(condition.getAgeGoe())
+        ).offset(pageable.getOffset())
+        .limit(pageable.getPageSize())
+        .fetch();
+
+    Long totalCount = queryFactory
+        .select(Wildcard.count)
+        .from(member)
+        .leftJoin(member.team, team)
+        .where(
+            usernameEq(condition.getUsername()),
+            teamNameEq(condition.getTeamName()),
+            ageLoe(condition.getAgeLoe()),
+            ageGoe(condition.getAgeGoe())
+        ).fetch().get(0);
+
+    return new PageImpl<>(result, pageable, totalCount);
   }
 
   private BooleanExpression usernameEq(String username) {
