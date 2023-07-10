d2e8b8f3e7c3b81ba07094901928bbf575002e1c
pythonstrup <pythonstrup@gmail.com>
2023-07-10 21:30:24 +0900

    QueryDSL - Where Parameter

--- JPA/querydsl/src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ JPA/querydsl/src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -4,6 +4,7 @@ import static study.querydsl.entity.QMember.member;
 import static study.querydsl.entity.QTeam.team;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import java.util.List;
@@ -87,5 +88,45 @@ public class MemberJpaRepository {
         .leftJoin(member.team, team)
         .where(builder)
         .fetch();
+  }
+
+  public List<MemberTeamDto> search(MemberSearchCondition condition) {
+
+    return queryFactory
+        .select(new QMemberTeamDto(
+            member.id.as("memberId"),
+            member.username,
+            member.age,
+            team.id.as("teamId"),
+            team.name.as("teamName")
+        ))
+        .from(member)
+        .leftJoin(member.team, team)
+        .where(
+            usernameEq(condition.getUsername()),
+            teamNameEq(condition.getTeamName()),
+            ageBetween(condition.getAgeLoe(), condition.getAgeGoe())
+        )
+        .fetch();
+  }
+
+  private BooleanExpression ageBetween(int ageLoe, int ageGoe) {
+    return ageLoe(ageLoe).and(ageGoe(ageGoe));
+  }
+
+  private BooleanExpression usernameEq(String username) {
+    return StringUtils.hasText(username) ? member.username.eq(username): null;
+  }
+
+  private BooleanExpression teamNameEq(String teamName) {
+    return StringUtils.hasText(teamName) ? team.name.eq(teamName): null;
+  }
+
+  private BooleanExpression ageGoe(Integer ageGoe) {
+    return ageGoe != null ? member.age.goe(ageGoe): null;
+  }
+
+  private BooleanExpression ageLoe(Integer ageLoe) {
+    return ageLoe != null ? member.age.loe(ageLoe): null;
   }
 }
