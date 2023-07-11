6b30deb4f1caaeb9910236d4a94ae9c2895105ff
pythonstrup <pythonstrup@gmail.com>
2023-07-11 21:07:24 +0900

    QueryDSL - QuerydslPredicateExecutor

--- JPA/querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ JPA/querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -1,6 +1,7 @@
 package study.querydsl.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static study.querydsl.entity.QMember.member;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -90,5 +91,33 @@ class MemberRepositoryTest {
     Page<MemberTeamDto> result = memberRepository.searchPageSimple(condition, pageRequest);
     assertThat(result.getSize()).isEqualTo(3);
     assertThat(result.getContent()).extracting("username").containsExactly("member1", "member2", "member3");
+  }
+
+
+  // 조인이 불가능하다. (묵시적 조인은 가능하지만 left join이 불가능하다.)
+  // 클라이언트가 QueryDSL에 의존해야한다. 서비스 클래스가 QueryDSL이라는 구현 기술에 의존해야 한다.
+  // 한계가 명확하기 때문에 복잡한 실무 환경에서 사용하기에는 어려워보인다.
+  @Test
+  void querydslPredicateExecutorTest() {
+    Team teamA = new Team("teamA");
+    Team teamB = new Team("teamB");
+    em.persist(teamA);
+    em.persist(teamB);
+
+    Member member1 = new Member("member1", 10, teamA);
+    Member member2 = new Member("member2", 20, teamA);
+
+    Member member3 = new Member("member3", 30, teamB);
+    Member member4 = new Member("member4", 40, teamB);
+    em.persist(member1);
+    em.persist(member2);
+    em.persist(member3);
+    em.persist(member4);
+
+    Iterable<Member> result = memberRepository.findAll(
+        member.age.between(20, 40).and(member.username.eq("member3")));
+    for (Member member : result) {
+      System.out.println("member = " + member);
+    }
   }
 }
