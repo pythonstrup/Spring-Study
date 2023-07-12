467fede1228600533704c3f0ede3a73024811367
pythonstrup <pythonstrup@gmail.com>
2023-07-12 23:12:12 +0900

    Advanced - V0

---
+++ spring-advanced/spring-advanced/src/main/java/hello/springadvanced/app/v0/OrderRepositoryV0.java
@@ -0,0 +1,24 @@
+package hello.springadvanced.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+  public void save(String itemId) {
+    if (itemId.equals("ex")) {
+      throw new IllegalStateException("Exception!!!");
+    }
+    sleep(1000);
+  }
+
+  private void sleep(int millis) {
+    try {
+      Thread.sleep(millis);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+}
