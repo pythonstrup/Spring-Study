7f1402a48c7a7f596d9ef5310dddc7f3aba5e68e
pythonstrup <pythonstrup@gmail.com>
2023-07-19 23:32:07 +0900

    Advanced - ThreadLocal

---
+++ spring-advanced/spring-advanced/src/test/java/hello/springadvanced/trace/threadlocal/ThreadLocalServiceTest.java
@@ -0,0 +1,45 @@
+package hello.springadvanced.trace.threadlocal;
+
+import hello.springadvanced.trace.threadlocal.code.FieldService;
+import hello.springadvanced.trace.threadlocal.code.ThreadLocalService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+
+  private final ThreadLocalService threadLocalService = new ThreadLocalService();
+
+  @Test
+  void field() {
+    log.info("main start");
+    Runnable userA = () -> {
+      threadLocalService.logic("userA");
+    };
+    Runnable userB = () -> {
+      threadLocalService.logic("userB");
+    };
+
+    Thread threadA = new Thread(userA);
+    threadA.setName("thread-A");
+    Thread threadB = new Thread(userB);
+    threadB.setName("thread-B");
+
+    threadA.start();
+//    sleep(2000); // 동시성 문제가 발생하지 않는 코드
+    sleep(100); // 동시성 문제
+    threadB.start();
+
+    sleep(2000); // 메인 쓰레드 종료 대기
+    log.info("main exit");
+  }
+
+
+  private void sleep(int millis) {
+    try {
+      Thread.sleep(millis);
+    }  catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+}
