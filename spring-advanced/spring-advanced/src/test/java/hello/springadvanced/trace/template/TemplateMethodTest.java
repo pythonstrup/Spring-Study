bfde17696d990e8bdf840a882ccc24c34697e3be
pythonstrup <pythonstrup@gmail.com>
2023-07-20 08:32:19 +0900

    Advanced - Template Method Pattern Test

--- spring-advanced/spring-advanced/src/test/java/hello/springadvanced/trace/template/TemplateMethodTest.java
+++ spring-advanced/spring-advanced/src/test/java/hello/springadvanced/trace/template/TemplateMethodTest.java
@@ -1,5 +1,8 @@
 package hello.springadvanced.trace.template;
 
+import hello.springadvanced.trace.template.code.AbstractTemplate;
+import hello.springadvanced.trace.template.code.SubClassLogic1;
+import hello.springadvanced.trace.template.code.SubClassLogic2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -34,5 +37,17 @@ public class TemplateMethodTest {
     long endTime = System.currentTimeMillis();
     long resultTime = endTime - startTime;
     log.info("resultTime={}", resultTime);
+  }
+
+  /**
+   * 템플릿 메소드 패턴 적용
+   */
+  @Test
+  void templateMethodV1() {
+    AbstractTemplate template1 = new SubClassLogic1();
+    template1.execute();
+
+    AbstractTemplate template2 = new SubClassLogic2();
+    template2.execute();
   }
 }
