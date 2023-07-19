a4e1bdb2c68ad65daa212b0de8689f85e4314667
pythonstrup <pythonstrup@gmail.com>
2023-07-19 23:38:44 +0900

    Advanced - ThreadLocalLogTrace

--- spring-advanced/spring-advanced/src/main/java/hello/springadvanced/config/LogTraceConfig.java
+++ spring-advanced/spring-advanced/src/main/java/hello/springadvanced/config/LogTraceConfig.java
@@ -2,14 +2,20 @@ package hello.springadvanced.config;
 
 import hello.springadvanced.trace.logtrace.FieldLogTrace;
 import hello.springadvanced.trace.logtrace.LogTrace;
+import hello.springadvanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LogTraceConfig {
 
+//  @Bean
+//  public LogTrace logTrace() {
+//    return new FieldLogTrace();
+//  }
+
   @Bean
   public LogTrace logTrace() {
-    return new FieldLogTrace();
+    return new ThreadLocalLogTrace();
   }
 }
