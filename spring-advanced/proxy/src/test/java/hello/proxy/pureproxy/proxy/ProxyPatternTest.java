dc39a402b9bdd4e56b1f150c86243611aaedfde3
pythonstrup <pythonstrup@gmail.com>
2023-08-14 22:53:36 +0900

    Proxy Pattern - CacheProxy

--- spring-advanced/proxy/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
+++ spring-advanced/proxy/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -1,15 +1,28 @@
 package hello.proxy.pureproxy.proxy;
 
+import hello.proxy.pureproxy.proxy.code.CacheProxy;
 import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
 import hello.proxy.pureproxy.proxy.code.RealSubject;
 import org.junit.jupiter.api.Test;
 
 public class ProxyPatternTest {
 
+  // 총 3초 걸림
   @Test
   void noProxyTest() {
     RealSubject realSubject = new RealSubject();
     ProxyPatternClient client = new ProxyPatternClient(realSubject);
+    client.execute();
+    client.execute();
+    client.execute();
+  }
+
+  // 캐시를 사용해 실제 객체를 한 번만 호출 => 1초 소요
+  @Test
+  void cacheProxyTest() {
+    RealSubject realSubject = new RealSubject();
+    CacheProxy cacheProxy = new CacheProxy(realSubject);
+    ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
     client.execute();
     client.execute();
     client.execute();
