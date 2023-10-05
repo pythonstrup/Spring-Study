f1dae242d09583b133c51929d13e3139fe5b8dce
pythonstrup <pythonstrup@gmail.com>
2023-10-05 22:33:14 +0900

    Reflection 추가

--- spring-advanced/proxy/src/test/java/hello/proxy/jdkdynamic/ReflectionTest.java
+++ spring-advanced/proxy/src/test/java/hello/proxy/jdkdynamic/ReflectionTest.java
@@ -4,6 +4,10 @@ import java.lang.reflect.Method;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+/**
+ * 리플렉션은 가급적 사용하지 않는 것이 좋다.
+ * - 런타임에 동작하기 때문에 컴파일 타임에 오류를 잡을 수 없다.
+ */
 @Slf4j
 public class ReflectionTest {
 
@@ -41,8 +45,30 @@ public class ReflectionTest {
     log.info("result2={}", result2);
   }
 
+  @Test
+  void reflection2() throws Exception {
+    // 클래스 정보
+    Class classHello = Class.forName("hello.proxy.jdkdynamic.ReflectionTest$Hello");
+    Hello target = new Hello();
+
+    // callA 메소드 정보
+    Method methodCallA = classHello.getMethod("callA");
+    dynamicCall(methodCallA, target);
+
+    // callB 메소드 정보
+    Method methodCallB = classHello.getMethod("callB");
+    dynamicCall(methodCallB, target);
+  }
+
+  private void dynamicCall(Method method, Object target) throws Exception {
+    log.info("start");
+    Object result = method.invoke(target);
+    log.info("result={}", result);
+  }
+
   @Slf4j
   static class Hello {
+
     public String callA() {
       log.info("callA");
       return "A";
