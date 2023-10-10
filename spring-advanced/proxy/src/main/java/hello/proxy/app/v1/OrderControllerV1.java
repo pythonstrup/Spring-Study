85bb4e9709ee7af763b7eb03d7abc6d08bfa13b1
pythonstrup <pythonstrup@gmail.com>
2023-10-10 22:41:34 +0900

    Dynamic Proxy 적용

--- spring-advanced/proxy/src/main/java/hello/proxy/app/v1/OrderControllerV1.java
+++ spring-advanced/proxy/src/main/java/hello/proxy/app/v1/OrderControllerV1.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public interface OrderControllerV1 {
 
   @GetMapping("/v1/request")
-  String request(@RequestParam String itemId);
+  String request(@RequestParam(name = "itemId") String itemId);
 
   @GetMapping("v1/no-log")
   String noLog();
