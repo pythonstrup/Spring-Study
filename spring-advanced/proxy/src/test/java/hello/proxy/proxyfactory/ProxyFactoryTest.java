e5775c3e9b0bd2f9b8206b4833eff5e13f3bd084
pythonstrup <pythonstrup@gmail.com>
2023-10-11 22:42:46 +0900

    ProxyFactory ProxyTargetClass

--- spring-advanced/proxy/src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
+++ spring-advanced/proxy/src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
@@ -49,5 +49,21 @@ public class ProxyFactoryTest {
     assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
   }
 
+  @Test
+  @DisplayName("ProxyTargetClass 옵션을 사용하면 인터페이스가 있어도 CGLIB를 사용하고, 클래스 기반 프록시 사용")
+  void proxyTargetClass() {
+    ServiceInterface target = new ServiceImpl();
+    ProxyFactory proxyFactory = new ProxyFactory(target);
+    proxyFactory.setProxyTargetClass(true); // 구체클래스인 ServiceImpl를 사용해버림
+    proxyFactory.addAdvice(new TimeAdvice());
+    ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+    log.info("targetClass={}", target.getClass());
+    log.info("proxyClass={}", proxy.getClass());
 
+    proxy.save();
+
+    assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+    assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+    assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
+  }
 }
