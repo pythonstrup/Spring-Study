67e0d4f736d2e0d5d08d684e1acd8c40032dc219
pythonstrup <pythonstrup@gmail.com>
2023-11-29 23:43:03 +0900

    DataSource - Connection Pool

--- jdbc/jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ jdbc/jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -4,6 +4,7 @@ import static hello.jdbc.connection.ConnectionConst.PASSWORD;
 import static hello.jdbc.connection.ConnectionConst.URL;
 import static hello.jdbc.connection.ConnectionConst.USERNAME;
 
+import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -29,9 +30,33 @@ public class ConnectionTest {
     useDataSource(dataSource);
   }
 
+  @Test
+  void dataSourceConnectionPool() throws SQLException, InterruptedException {
+    // 커넥션 풀링
+    HikariDataSource dataSource = new HikariDataSource();
+    dataSource.setJdbcUrl(URL);
+    dataSource.setUsername(USERNAME);
+    dataSource.setPassword(PASSWORD);
+    dataSource.setMaximumPoolSize(10); // default = 10
+    dataSource.setPoolName("myPool");
+
+    useDataSource(dataSource);
+    Thread.sleep(1000);
+  }
+
   private void useDataSource(DataSource dataSource) throws SQLException {
     Connection con1 = dataSource.getConnection();
     Connection con2 = dataSource.getConnection();
+    Connection con3 = dataSource.getConnection();
+    Connection con4 = dataSource.getConnection();
+    Connection con5 = dataSource.getConnection();
+    Connection con6 = dataSource.getConnection();
+    Connection con7 = dataSource.getConnection();
+    Connection con8 = dataSource.getConnection();
+    Connection con9 = dataSource.getConnection();
+    Connection con10 = dataSource.getConnection();
+    // 만약 풀이 모두 사용 중이라면 풀이 생길 때까지 대기한다. => 그런데 close가 없기 때문에 계속 대기상태가 될 것이다.
+//    Connection con11 = dataSource.getConnection();
     log.info("connection={}, class={}", con1, con1.getClass());
     log.info("connection={}, class={}", con2, con2.getClass());
   }
