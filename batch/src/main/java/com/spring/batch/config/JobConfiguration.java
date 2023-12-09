2777942751ae538f405fd8db4a25db41da551272
pythonstrup <pythonstrup@gmail.com>
2023-12-09 15:38:57 +0900

    JobInstance

--- batch/src/main/java/com/spring/batch/config/JobConfiguration.java
+++ batch/src/main/java/com/spring/batch/config/JobConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class DBJobConfiguration {
+public class JobConfiguration {
 
   private final JobBuilderFactory jobBuilderFactory;
   private final StepBuilderFactory stepBuilderFactory;
@@ -27,24 +27,18 @@ public class DBJobConfiguration {
   @Bean
   public Step step1() {
     return stepBuilderFactory.get("step1")
-        .tasklet(((contribution, chunkContext) -> {
-          System.out.println("=========================");
-          System.out.println(" >> step1 was executed!!!");
-          System.out.println("=========================");
+        .tasklet((contribution, chunkContext) -> {
+          System.out.println("step1 was executed");
           return RepeatStatus.FINISHED;
-        }))
-        .build();
+        }).build();
   }
 
   @Bean
   public Step step2() {
-    return stepBuilderFactory.get("step2")
-        .tasklet(((contribution, chunkContext) -> {
-          System.out.println("=========================");
-          System.out.println(" >> step2 was executed!!!");
-          System.out.println("=========================");
+    return stepBuilderFactory.get("step1")
+        .tasklet((contribution, chunkContext) -> {
+          System.out.println("step2 was executed");
           return RepeatStatus.FINISHED;
-        }))
-        .build();
+        }).build();
   }
 }
