247c036e0ca8cb6d1dd0b671a68155bbca6a6616
pythonstrup <pythonstrup@gmail.com>
2023-12-21 22:48:40 +0900

    Domain-Driven Design Start

---
+++ ddd/src/test/java/com/pythonstrup/ddd/DddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pythonstrup.ddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
