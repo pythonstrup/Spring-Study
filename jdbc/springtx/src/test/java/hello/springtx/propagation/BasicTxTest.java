88257a428c91e3f94d5e3734a07903549e0c7946
pythonstrup <pythonstrup@gmail.com>
2024-01-02 08:28:57 +0900

    Transaction Propagation - REQUIRES_NEW(물리 트랜잭션 분리)

--- jdbc/springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ jdbc/springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -11,8 +11,10 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.UnexpectedRollbackException;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 @Slf4j
@@ -119,5 +121,25 @@ public class BasicTxTest {
     log.info("외부 트랜잭션 커밋");
     assertThatThrownBy(() ->txManager.commit(outer))
         .isInstanceOf(UnexpectedRollbackException.class);
+  }
+
+  @Test
+  @Transactional()
+  void inner_rollback_requires_new() {
+    log.info("외부 트랜잭션 시작");
+    TransactionStatus outer = txManager.getTransaction(new DefaultTransactionDefinition());
+    log.info("outer.isNewTransaction()={}", outer.isNewTransaction()); // true
+
+    log.info("내부 트랜잭션 시작");
+    DefaultTransactionDefinition definition = new DefaultTransactionDefinition();
+    definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+    TransactionStatus inner = txManager.getTransaction(definition);
+    log.info("inner.isNewTransaction()={}", inner.isNewTransaction()); // true => 새로운 물리 트랜잭션
+    // 두번째 커넥션(내부 트랜잭션)을 사용하는 동안 첫번째 커넥션(외부 트랜잭션)은 보류 상태가 된다.
+
+    log.info("내부 트랜잭션 롤백");
+    txManager.rollback(inner); // 내부만 롤백, 내부 트랜잭션 종료 및 두번째 커넥션을 커넥션 풀에 반납
+    log.info("외부 트랜잭션 커밋"); // 보류되었던 첫번째 커넥션(외부 트랜잭션)의 보류가 끝나고 다시 사용
+    txManager.commit(outer); // 물리 트랜잭션이 분리되었기 때문에 문제가 발생하지 않는다.
   }
 }
