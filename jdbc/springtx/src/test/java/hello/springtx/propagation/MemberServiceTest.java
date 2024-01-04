4c71fef182ad9c784a10569f8cf32d9d485e998f
pythonstrup <pythonstrup@gmail.com>
2024-01-04 09:02:12 +0900

    Propagation Ex - REQUIRES_NEW

--- jdbc/springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ jdbc/springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -127,4 +127,22 @@ class MemberServiceTest {
     assertTrue(memberRepository.find(username).isEmpty());
     assertTrue(logRepository.find(username).isEmpty());
   }
+
+  /**
+   * memberService    @Transactional: ON
+   * memberRepository @Transactional: ON
+   * logRepository    @Transactional: ON(REQUIRES_NEW) Exception
+   */
+  @Test
+  void recoverException_success() {
+    // given
+    String username = "로그예외_recoverException_fail";
+
+    // when
+    memberService.joinV2(username);
+
+    // then: member는 커밋되길 기대했지만! 모든 데이터가 롤백된다.
+    assertTrue(memberRepository.find(username).isPresent());
+    assertTrue(logRepository.find(username).isEmpty());
+  }
 }
