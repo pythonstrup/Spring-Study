0161b9692bacfde09f0be1703babf8eb30b90696
pythonstrup <pythonstrup@gmail.com>
2024-01-07 14:52:04 +0900

    [Spring Batch] StepExecution

--- batch/src/main/java/com/spring/batch/config/StepExecutionConfiguration.java
+++ batch/src/main/java/com/spring/batch/config/StepExecutionConfiguration.java
@@ -1,7 +1,7 @@
 package com.spring.batch.config;
 
-import com.spring.batch.common.CustomTasklet;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
@@ -10,34 +10,51 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Slf4j
+@Configuration
 @RequiredArgsConstructor
-public class StepConfiguration {
+public class StepExecutionConfiguration {
 
   private final JobBuilderFactory jobBuilderFactory;
   private final StepBuilderFactory stepBuilderFactory;
 
-//  @Bean
+  @Bean
   public Job job() {
     return jobBuilderFactory.get("job")
         .start(step1())
         .next(step2())
+        .next(step3())
         .build();
   }
 
-//  @Bean
+  @Bean
   public Step step1() {
     return stepBuilderFactory.get("step1")
-        .tasklet((contribution, chunkContext) -> {
-          System.out.println("step1 was executed");
+        .tasklet(((contribution, chunkContext) -> {
+          log.info(">> step1 has executed");
           return RepeatStatus.FINISHED;
-        }).build();
+        }))
+        .build();
   }
 
-//  @Bean
+  @Bean
   public Step step2() {
     return stepBuilderFactory.get("step2")
-        .tasklet(new CustomTasklet())
+        .tasklet(((contribution, chunkContext) -> {
+          log.info(">> step2 has executed");
+//          throw new RuntimeException("step2 has failed"); // Step2에서 실패해버리면 Step3는 실행되지도 않는다.
+          return RepeatStatus.FINISHED; // Step2에서 실패한 상태가 DB에 담겨있을 때, 다시 실행하면 Step1은 실행되지 않고 Step2와 Step3만 실행된다.
+        }))
+        .build();
+  }
+
+  @Bean
+  public Step step3() {
+    return stepBuilderFactory.get("step3")
+        .tasklet(((contribution, chunkContext) -> {
+          log.info(">> step3 has executed");
+          return RepeatStatus.FINISHED;
+        }))
         .build();
   }
 }
