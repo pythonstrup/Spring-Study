b9dd9f7911fcbc85e4b1022d52f44bd72b63b8fc
pythonstrup <pythonstrup@gmail.com>
2024-03-28 21:39:43 +0900

    [Test] 주문 통계 요구사항 구현

--- test/cafekiosk/src/main/java/sample/cafekiosk/spring/domain/order/OrderRepository.java
+++ test/cafekiosk/src/main/java/sample/cafekiosk/spring/domain/order/OrderRepository.java
@@ -1,5 +1,16 @@
 package sample.cafekiosk.spring.domain.order;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {}
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+  @Query("select o from Order o "
+      + "where o.registeredAt >= :startDateTime "
+      + "and o.registeredAt < :endDateTime "
+      + "and o.orderStatus = :orderStatus")
+  List<Order> findOrdersBy(
+      LocalDateTime startDateTime, LocalDateTime endDateTime, OrderStatus orderStatus);
+}
