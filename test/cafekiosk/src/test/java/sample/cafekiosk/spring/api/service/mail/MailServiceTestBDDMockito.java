392b57723bb1fc3651ed52d7855e05e7ac32dbc9
pythonstrup <pythonstrup@gmail.com>
2024-03-29 22:30:30 +0900

    [Test] BDDMockito

--- test/cafekiosk/src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTestBDDMockito.java
+++ test/cafekiosk/src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTestBDDMockito.java
@@ -10,6 +10,7 @@ import static org.mockito.Mockito.when;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -18,7 +19,7 @@ import sample.cafekiosk.spring.domain.history.mail.MailSendHistory;
 import sample.cafekiosk.spring.domain.history.mail.MailSendHistoryRepository;
 
 @ExtendWith(MockitoExtension.class)
-class MailServiceTestMockAnnotation {
+class MailServiceTestBDDMockito {
 
   @Mock
   MailSendClient mailSendClient;
@@ -33,8 +34,13 @@ class MailServiceTestMockAnnotation {
   @DisplayName("메일 전송 테스트")
   public void sendMail() {
     // given
-    when(mailService.sendMail(anyString(), anyString(), anyString(), anyString()))
-        .thenReturn(true);
+//    when(mailService.sendMail(anyString(), anyString(), anyString(), anyString()))
+//        .thenReturn(true);
+
+    // BDD에 맞게 Mockito를 사용하기 위해 쓰인다.
+    // 사실 Mockito 객체와 기능은 동일하나 이름만 다르다.
+    BDDMockito.given(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
+        .willReturn(true);
 
     // when
     boolean result = mailService.sendMail("", "", "", "");
--- test/cafekiosk/src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTestSpyAnnotation.java
+++ test/cafekiosk/src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTestSpyAnnotation.java
@@ -3,24 +3,25 @@ package sample.cafekiosk.spring.api.service.mail;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import sample.cafekiosk.spring.client.mail.MailSendClient;
 import sample.cafekiosk.spring.domain.history.mail.MailSendHistory;
 import sample.cafekiosk.spring.domain.history.mail.MailSendHistoryRepository;
 
 @ExtendWith(MockitoExtension.class)
-class MailServiceTestMockAnnotation {
+class MailServiceTestSpyAnnotation {
 
-  @Mock
+  @Spy
   MailSendClient mailSendClient;
 
   @Mock
@@ -33,8 +34,11 @@ class MailServiceTestMockAnnotation {
   @DisplayName("메일 전송 테스트")
   public void sendMail() {
     // given
-    when(mailService.sendMail(anyString(), anyString(), anyString(), anyString()))
-        .thenReturn(true);
+    // spy는 그냥 when을 사용할 수 없다.
+    // sendMail만 Stubbing 되고 나머지 메소드 a, b, c는 정상적으로 작동한다.
+    doReturn(true)
+        .when(mailSendClient)
+        .sendEmail(anyString(), anyString(), anyString(), anyString());
 
     // when
     boolean result = mailService.sendMail("", "", "", "");
