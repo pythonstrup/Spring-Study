1e21756fa4a91c3ce736f7c97141d17f894125d7
pythonstrup <pythonstrup@gmail.com>
2024-04-03 23:36:20 +0900

    [Test] Custom Tag

--- test/the-java-test/src/test/java/com/test/thejavatest/StudyTestCustomAnnotation.java
+++ test/the-java-test/src/test/java/com/test/thejavatest/StudyTestCustomAnnotation.java
@@ -2,23 +2,21 @@ package com.test.thejavatest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.test.thejavatest.annotation.FastTest;
+import com.test.thejavatest.annotation.SlowTest;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
 
-class StudyTestTagAndFilter {
+class StudyTestCustomAnnotation {
 
-  @Test
+  @FastTest
   @DisplayName("스터디 만들기 \uD83D\uDE31")
-  @Tag("fast")
   void create_new_study() {
     Study study = new Study(10);
     assertThat(study.getLimit()).isGreaterThan(0);
   }
 
-  @Test
+  @SlowTest
   @DisplayName("스터디 만들기 \uD83D\uDE31")
-  @Tag("slow")
   void create_new_study_again() {
     Study study = new Study(10);
     assertThat(study.getLimit()).isGreaterThan(0);
