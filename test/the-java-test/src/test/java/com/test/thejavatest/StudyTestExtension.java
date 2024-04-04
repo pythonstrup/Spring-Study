8e0976946a7fecb234b03a37e16a403dd39f7b14
pythonstrup <pythonstrup@gmail.com>
2024-04-05 00:00:33 +0900

    [Test] Test Extension

---
+++ test/the-java-test/src/test/java/com/test/thejavatest/StudyTestExtension.java
@@ -0,0 +1,26 @@
+package com.test.thejavatest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.test.thejavatest.annotation.SlowTest;
+import com.test.thejavatest.extension.FindSlowTestExtensionCustom;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+//@ExtendWith(FindSlowTestExtension.class) // 선언적 방법
+//@ExtendWith(FindSlowTestExtensionCustom.class) // 생성자에 인자를 전달해 인스턴스를 만들어야 하면 에러가 발생한다.
+class StudyTestExtension {
+
+  @RegisterExtension
+  static FindSlowTestExtensionCustom findSlowTestExtensionCustom =
+      new FindSlowTestExtensionCustom(1000L);
+
+//  @Test
+  @SlowTest
+  @DisplayName("테스트")
+  void create_study() throws InterruptedException {
+    Thread.sleep(1005L);
+    Study study = new Study(10);
+    assertThat(study.getLimit()).isGreaterThan(0);
+  }
+}
---
+++ test/the-java-test/src/test/java/com/test/thejavatest/extension/FindSlowTestExtension.java
@@ -0,0 +1,41 @@
+package com.test.thejavatest.extension;
+
+import com.test.thejavatest.annotation.SlowTest;
+import java.lang.reflect.Method;
+import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
+import org.junit.jupiter.api.extension.BeforeTestExecutionCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ExtensionContext.Namespace;
+import org.junit.jupiter.api.extension.ExtensionContext.Store;
+
+public class FindSlowTestExtension implements BeforeTestExecutionCallback, AfterTestExecutionCallback {
+
+  private static final long THRESHOLD = 1000L;
+
+  @Override
+  public void beforeTestExecution(ExtensionContext context) throws Exception {
+    Store store = getStore(context);
+    store.put("START TIME", System.currentTimeMillis());
+  }
+
+  @Override
+  public void afterTestExecution(ExtensionContext context) throws Exception {
+    Method requiredTestMethod = context.getRequiredTestMethod();
+    SlowTest annotation = requiredTestMethod.getAnnotation(SlowTest.class);
+
+    String testMethodName = requiredTestMethod.getName();
+    Store store = getStore(context);
+    Long startTime = store.remove("START TIME", long.class);
+    long duration = System.currentTimeMillis() - startTime;
+    if (duration > THRESHOLD && annotation == null) {
+      System.out.printf("Please consider mark method [%s] with @SlowTest.\n", testMethodName);
+    }
+  }
+
+  private Store getStore(ExtensionContext context) {
+    String testClassName = context.getRequiredTestClass().getName();
+    String testMethodName = context.getRequiredTestMethod().getName();
+    Store store = context.getStore(Namespace.create(testClassName, testMethodName));
+    return store;
+  }
+}
