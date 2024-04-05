bac38e58d846db1758c64ce8542a801bd7bdc688
pythonstrup <pythonstrup@gmail.com>
2024-04-05 21:39:19 +0900

    [Test] Mock Stubbing

--- test/the-java-test/src/main/java/com/test/thejavatest/domain/Member.java
+++ test/the-java-test/src/main/java/com/test/thejavatest/domain/Member.java
@@ -2,4 +2,22 @@ package com.test.thejavatest.domain;
 
 public class Member {
 
+  private Long id;
+  private String email;
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
 }
--- test/the-java-test/src/test/java/com/test/thejavatest/study/StudyServiceTest.java
+++ test/the-java-test/src/test/java/com/test/thejavatest/study/StudyServiceTest.java
@@ -1,11 +1,20 @@
 package com.test.thejavatest.study;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
 
+import com.test.thejavatest.domain.Member;
+import com.test.thejavatest.domain.Study;
 import com.test.thejavatest.member.MemberService;
+import java.util.Optional;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -20,7 +29,62 @@ class StudyServiceTest {
   @Test
   void createStudyService() {
     StudyService studyService = new StudyService(memberService, studyRepository);
-    assertNotNull(studyService);
+
+    Member member = new Member();
+    member.setId(1L);
+    member.setEmail("bell@mobidoc.us");
+
+    when(memberService.findById(anyLong())).thenReturn(Optional.of(member));
+
+    Study study = new Study(10, "java");
+    studyService.createNewStudy(1L, study);
+  }
+
+  @Test
+  void exceptionStubbing() {
+    StudyService studyService = new StudyService(memberService, studyRepository);
+
+    Member member = new Member();
+    member.setId(1L);
+    member.setEmail("bell@mobidoc.us");
+
+    when(memberService.findById(anyLong())).thenThrow(new RuntimeException());
+
+    Study study = new Study(10, "java");
+    assertThatThrownBy(() -> studyService.createNewStudy(1L, study))
+        .isInstanceOf(RuntimeException.class);
+  }
+
+  @Test
+  void validate() {
+    Mockito.doThrow(new IllegalArgumentException()).when(memberService).validate(1L);
+    assertThatThrownBy(() -> memberService.validate(1L))
+        .isInstanceOf(IllegalArgumentException.class);
+    memberService.validate(2L);
+  }
+
+  @Test
+  void createStudy() {
+    StudyService studyService = new StudyService(memberService, studyRepository);
+
+    Member member = new Member();
+    member.setId(1L);
+    member.setEmail("bell@mobidoc.us");
+
+    when(memberService.findById(anyLong()))
+        .thenReturn(Optional.of(member))
+        .thenThrow(new RuntimeException())
+        .thenReturn(Optional.empty());
+
+    Optional<Member> member1 = memberService.findById(1L);
+    assertThat(member1).isPresent();
+    assertThat(member1.get().getId()).isEqualTo(1L);
+
+    assertThatThrownBy(() -> memberService.findById(1L))
+        .isInstanceOf(RuntimeException.class);
+
+    Optional<Member> member3 = memberService.findById(1L);
+    assertThat(member3).isEmpty();
   }
 
   // 아래와 같이 파라미터로 넣어줘도 된다.
