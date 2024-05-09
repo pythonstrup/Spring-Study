b49ad0be0e9d33929792a684bfb84fd540962960
pythonstrup <pythonstrup@gmail.com>
2024-05-09 23:15:05 +0900

    [Security] SecurityContext

--- security/src/main/java/com/security/IndexController.java
+++ security/src/main/java/com/security/IndexController.java
@@ -1,22 +1,41 @@
 package com.security;
 
+import com.security.authentication.service.SecurityContextService;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 
+  private final SecurityContextService securityContextService;
+
+  public IndexController(SecurityContextService securityContextService) {
+    this.securityContextService = securityContextService;
+  }
+
   @GetMapping
-  public String index(String customParam) {
-    if (customParam != null) {
-      return "customParam";
-    }
+  public String index() {
+    SecurityContext context = SecurityContextHolder.getContextHolderStrategy().getContext();
+    Authentication authentication = context.getAuthentication();
+    System.out.println("authentication = " + authentication);
+
+    securityContextService.securityContext();
+
     return "index";
   }
+
+//  @GetMapping
+//  public String index(String customParam) {
+//    if (customParam != null) {
+//      return "customParam";
+//    }
+//    return "index";
+//  }
 
   @GetMapping("/home")
   public String home() {
--- security/src/main/java/com/security/authentication/SecurityConfig_securityContext.java
+++ security/src/main/java/com/security/authentication/SecurityConfig_securityContext.java
@@ -1,7 +1,8 @@
-package com.security.certification;
+package com.security.authentication;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -12,26 +13,17 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 import org.springframework.security.web.savedrequest.SavedRequest;
 
-//@Configuration
-//@EnableWebSecurity
-public class SecurityConfig_requestCache {
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig_securityContext {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
-    requestCache.setMatchingRequestParameterName("customParam=y");
     http
         .authorizeHttpRequests(auth -> auth
             .requestMatchers("/logout/success").permitAll()
             .anyRequest().authenticated())
-        .formLogin(form -> form
-            .successHandler(((request, response, authentication) -> {
-              SavedRequest savedRequest = requestCache.getRequest(request, response);
-              String redirectUrl = savedRequest.getRedirectUrl();
-              response.sendRedirect(redirectUrl);
-            })))
-        .requestCache(cache -> cache
-            .requestCache(requestCache));
+        .formLogin(Customizer.withDefaults());
     return http.build();
   }
 
